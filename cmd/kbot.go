d49cdf679bfc74b7366937a023eed4da02ce76f7
ASEMUA <asem@ukr.net>
2024-04-03 16:58:53 +0000

    V1.1.0

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -19,9 +19,9 @@ var (
 
 // kbotCmd represents the kbot command
 var kbotCmd = &cobra.Command{
-	Use:   "kbot",
+	Use:     "kbot",
 	Aliases: []string{"start"},
-	Short: "A brief description of your command",
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -44,11 +44,11 @@ to quickly create a Cobra application.`,
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 			log.Print(m.Message().Payload, m.Text())
 			payload := m.Message().Payload
-			
+
 			switch payload {
-			  case "hello":
+			case "hello":
 				err = m.Send(fmt.Sprintf("Hello I`m Kbot %s", appVersion))
-			  case "date":
+			case "date":
 				err = m.Send(fmt.Sprintf("Hello! Today is: %s", time.Now()))
 			}
 			return err
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/ASEMUA/kbot
 
-go 1.22.1
+go 1.21.1
 
 require (
 	github.com/spf13/cobra v1.8.0
